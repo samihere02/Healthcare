ffbd0fb867da63ca6a76cb7f1d99030f3372fdf7
samihere02 <letsmailmuhammadsamiii@gmail.com>
2025-02-04 16:23:48 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Add any necessary interactivity here
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.cta-button');
+    button.addEventListener('click', () => {
+        alert('Thank you for your interest! We will be in touch soon.');
+    });
+});
